861a1bd93d236a13917f3642d5a73265153800a8
enzomarc <emarc237@gmail.com>
2019-01-11 06:36:53 +0100

    Update atom.php

--- atom.php
+++ atom.php
@@ -3,4 +3,11 @@
 echo "\nYour Atom Application is serving on localhost:2019\n";
 echo "Open http://localhost:2019 in your web browser to access your application\n\n";
 
-return exec("php -S localhost:2019 -t public");
+if (!file_exists('config/env.json')) {
+    copy('config/env.example.json', 'config/env.json');
+}
+
+exec("php -S localhost:2019 -t public");
+
+echo "\nYour Atom Application is serving on localhost:2019\n";
+echo "Open http://localhost:2019 in your web browser to access your application\n\n";
